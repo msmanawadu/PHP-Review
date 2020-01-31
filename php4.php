1bb386c8d708640972f2133707541e379e3889c4
Sandeepa Manawadu <msmanawadu@gmail.com>
2020-01-31 09:32:44 +0530

    echo() exercise 3

---
+++ php4.php
@@ -0,0 +1,8 @@
+<html><head></head>
+<body>
+    <?php 
+    echo("<h1 align = center> <i> <font color = red> National Institute of Business Management </font> </i> </h1>");
+    echo("<h2 align = center> DCSD 14.3 </h2>");
+    ?>
+</body>
+</html>

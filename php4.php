afbd623a7b4bc761b0edffa9dab31cf5fd55c738
Sandeepa Manawadu <msmanawadu@gmail.com>
2020-02-01 07:32:12 +0530

    listing all array elements using foreach()

--- php4.php
+++ php4.php
@@ -4,10 +4,14 @@
     //Associative array - method 2
     $fruit["red"] = "Apples";
     $fruit["orange"] = "Oranges";
-   $fruit["yellow"] = "Pineapple";
+    $fruit["yellow"] = "Pineapple";
+    $food = $fruit["red"];
+    echo("I love to eat $food");
 
-   $food = $fruit["red"];
-   echo("I love to eat $food");
+    //Listing all array elements 
+    foreach($fruit as $product){
+        echo("<li> I like to eat $product </li>");
+    }
 ?>
 </body>
 </html>

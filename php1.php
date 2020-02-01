6fa0ebd45aa05c1d2b2a3e542ff30b17277d9eb4
Sandeepa Manawadu <msmanawadu@gmail.com>
2020-02-01 07:11:12 +0530

    listing all array elements using foreach()

--- php1.php
+++ php1.php
@@ -3,7 +3,12 @@
 <?php
     // Method 1 of numeric arrays 
      $color = array("red", "blue", "green");
-     echo("My favourite color is $color[1]");
+     echo("My favourite color is $color[1] <br>");
+
+     //Listing all array elements
+     foreach($color as $col){
+        echo("<li> I like color $col </li>");
+     }
 ?>
 </body>
 </html>

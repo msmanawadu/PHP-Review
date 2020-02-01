e27324147678b6de3e35bfecb2dbddd83ea19035
Sandeepa Manawadu <msmanawadu@gmail.com>
2020-02-01 07:24:57 +0530

    listing all array elements using foreach()

--- php3.php
+++ php3.php
@@ -5,6 +5,11 @@
     $province = array("western"=>"colombo", "southern"=>"matara");
     $city = $province["southern"];
     echo("I live in $city");
+
+    //Listing all array elements
+    foreach($province as $pro) {
+        echo("<li> I live in $pro </li>");
+    } 
 ?>
 </body>
 </html>

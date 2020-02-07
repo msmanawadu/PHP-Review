901aae094ec46dd4d7f38c64342af76134d490df
Sandeepa Manawadu <msmanawadu@gmail.com>
2020-02-07 09:59:13 +0530

    handling form data exercise 1

--- one/receiver.php
+++ one/receiver.php
@@ -1,15 +1,9 @@
 <html><head></head>
 <body>
-<h1> Receiving the data </h1>    
-<?php
-    //Associative array 
-    $userId = $_REQUEST["userid"];
-    $userName = $_REQUEST["username"];
-    $status = $_REQUEST["status"];
-
-    echo("User ID: $userId <br>");
-    echo("Username: $userName <br>");
-    echo("Status: $status");
-?>
+<?php 
+$name = $_REQUEST["uName"];
+$pwd = $_REQUEST["pWord"];
+echo("Username : $name"."<br>"."Password : $pwd");
+?>    
 </body>
 </html>

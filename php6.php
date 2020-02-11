d2cb080b589380a05ba249f0fd6fca609dbde234
Sandeepa Manawadu <msmanawadu@gmail.com>
2020-02-11 21:18:19 +0530

    setting and retrieving the value of a cookie

--- php6.php
+++ php6.php
@@ -1,8 +1,9 @@
 <?php
-    setcookie("Good","Be a good student, be happy",time()+3600);
+    setcookie("message","Be happy for 1 hour",time()+3600);
 ?>
 <html><head></head>
 <body>
 <h1>Cookie Written</h1>
+<a href = "http://localhost:8888/php7.php"> Click here to view Cookie </a>
 </body>
 </html>
---
+++ php7.php
@@ -0,0 +1,9 @@
+<html><head></head>
+<body>
+<h2><u>Cookie Value</u></h2>
+<?php 
+    $value = $_COOKIE["message"];
+    echo("Cookie value : $value");
+?>
+</body>
+</html>

50956f32229676d87314d215c0790dfb4ce9004d
Sandeepa Manawadu <msmanawadu@gmail.com>
2020-02-12 14:54:55 +0530

    Writing a record to a table

--- php6.php
+++ php6.php
@@ -1,9 +1,22 @@
-<?php
-    setcookie("message","Be happy for 1 hour",time()+3600);
-?>
 <html><head></head>
 <body>
-<h1>Cookie Written</h1>
-<a href = "http://localhost:8888/php7.php"> Click here to view Cookie </a>
+<h2>Writing a record to a table</h2>
+<?php
+    //make DB connection
+    $link = mysql_connect("localhost:8888","root","") or die("Error in DB connection");
+
+    //make DB selection
+    mysql_select_db("Web",$link) or die("Error in DB selection");
+
+    //Query execution
+    $query = "INSERT INTO 'Web'.'userinfo'('userid','uname','status')
+    values('100','Nimal','No')";
+
+    mysql_query($query) or die("Error in query execution.mysql_error();");
+?>
 </body>
 </html>
+
+
+
+
